774fa4de0857826ab501b20d571cefe1e753ed2c
milaq <micha.laqua@gmail.com>
2013-08-26 11:46:37 +0200

    make sure we only build when needed

--- Android.mk
+++ Android.mk
@@ -1,6 +1,3 @@
-ifndef WPA_SUPPLICANT_VERSION
-WPA_SUPPLICANT_VERSION := VER_0_6_X
-endif
-ifeq ($(WPA_SUPPLICANT_VERSION),VER_0_6_X)
+ifeq ($(WPA_SUPPLICANT_VERSION),VER_0_6_X_COMPAT)
     include $(call all-subdir-makefiles)
 endif
